c94d8e2310dfe881fefda2e8fa5f875199de589f
Your Name <user@example.com>
2025-08-03 00:35:30 +0530

    created successfully the working login and register page

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const connectDB = require('./config/db.js');
+const userRoutes = require('./routes/userRoutes.js');
+connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res)=>{
     res.send("Hello from the Smart Pantry API!");
